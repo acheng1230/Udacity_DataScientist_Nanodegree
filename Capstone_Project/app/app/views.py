7c23b50459c8d71c2cb32dc223b8508a8bb51021
Alex Cheng <acheng1230@gmail.com>
2020-08-18 14:40:10 -0700

    updates to app

--- Capstone_Project/app/app/views.py
+++ Capstone_Project/app/app/views.py
@@ -1,6 +1,8 @@
 from app import app
 from flask import render_template
 
+from nbapy.scoreboard import Scoreboard
+from wrangling_scripts.constants import CITY_TO_TEAM
 from wrangling_scripts.wrangle_data import get_games
 
 test_gameid = '0021901318'
@@ -8,10 +10,35 @@ test_date = '08-13-2020'
 
 @app.route("/")
 def index():
-    games = get_games(test_date)
-    games
-    return render_template("index.html", data_set=games)
+    return render_template("index.html")
 
-@app.route("/about")
-def about():
-    return "<h1 style='color:red'>About!!!</h1>"
+@app.route("/scores")
+def scores():
+    games = get_games(test_date)
+
+    winners = []
+    for i, value in games.iterrows():
+        if (value["HOME_TEAM_PTS"] > value["VISITOR_TEAM_PTS"]):
+            winners.append(value["HOME_TEAM_ABBREVIATION"])
+        elif (value["HOME_TEAM_PTS"] < value["VISITOR_TEAM_PTS"]):
+            winners.append(value["VISITOR_TEAM_ABBREVIATION"])
+        else:
+            winners.append(None)
+
+    return render_template("scores.html",
+                           games=games,
+                           winners=winners)
+
+@app.route('/standings')
+def standings():
+    """
+    NBA Standings Page (Western & Eastern Conferences)
+    """
+    scoreboard = Scoreboard()
+    east_standings = scoreboard.east_conf_standings_by_day()
+    west_standings = scoreboard.west_conf_standings_by_day()
+
+    return render_template("standings.html",
+                           east_standings=east_standings,
+                           west_standings=west_standings,
+                           team=CITY_TO_TEAM)
---
+++ Capstone_Project/app/wrangling_scripts/constants.py
@@ -0,0 +1,439 @@
+TEAM_NAME_TO_ID = {
+    'atlanta hawks': {
+        'id': '1610612737',
+    }, 'boston celtics': {
+        'id': '1610612738',
+    }, 'brooklyn nets': {
+        'id': '1610612751',
+    }, 'charlotte hornets': {
+        'id': '1610612766',
+    }, 'chicago bulls': {
+        'id': '1610612741',
+    }, 'cleveland cavaliers': {
+        'id': '1610612739',
+    }, 'dallas mavericks': {
+        'id': '1610612742',
+    }, 'denver nuggets': {
+        'id': '1610612743',
+    }, 'detroit pistons': {
+        'id': '1610612765',
+    }, 'golden state warriors': {
+        'id': '1610612744',
+    }, 'houston rockets': {
+        'id': '1610612745',
+    }, 'indiana pacers': {
+        'id': '1610612754',
+    }, 'los angeles clippers': {
+        'id': '1610612746',
+    }, 'los angeles lakers': {
+        'id': '1610612747',
+    }, 'memphis grizzlies': {
+        'id': '1610612763',
+    }, 'miami heat': {
+        'id': '1610612748',
+    }, 'milwaukee bucks': {
+        'id': '1610612749',
+    }, 'minnesota timberwolves': {
+        'id': '1610612750',
+    }, 'new orleans pelicans': {
+        'id': '1610612740',
+    }, 'new york knicks': {
+        'id': '1610612752',
+    }, 'oklahoma city': {
+        'id': '1610612760',
+    }, 'orlando magic': {
+        'id': '1610612753',
+    }, 'philadelphia sixers': {
+        'id': '1610612755',
+    }, 'phoenix suns': {
+        'id': '1610612756',
+    }, 'portland trail blazers': {
+        'id': '1610612757',
+    }, 'sacramento kings': {
+        'id': '1610612758',
+    }, 'san antonio spurs': {
+        'id': '1610612759',
+    }, 'toronto raptors': {
+        'id': '1610612761',
+    }, 'utah jazz': {
+        'id': '1610612762',
+    }, 'washington wizards': {
+        'id': '1610612764',
+    }
+}
+
+TEAM_ID_DATA = {
+    'ATL': {
+        'img': 'img/atl.png',
+        'city': 'Atlanta',
+        'name': 'Hawks',
+    }, 'BOS': {
+        'img': 'img/bos.png',
+        'city': 'Boston',
+        'name': 'Celtics',
+    }, 'BKN': {
+        'img': 'img/bkn.png',
+        'city': 'Brooklyn',
+        'name': 'Nets',
+    }, 'CHA': {
+        'img': 'img/cha.png',
+        'city': 'Charlotte',
+        'name': 'Hornets',
+    }, 'CHI': {
+        'img': 'img/chi.png',
+        'city': 'Chicago',
+        'name': 'Bulls',
+    }, 'CLE': {
+        'img': 'img/cle.png',
+        'city': 'Cleveland',
+        'name': 'Cavaliers',
+    }, 'DAL': {
+        'img': 'img/dal.png',
+        'city': 'Dallas',
+        'name': 'Mavericks',
+    }, 'DEN': {
+        'img': 'img/den.png',
+        'city': 'Denver',
+        'name': 'Nuggets',
+    }, 'DET': {
+        'img': 'img/det.png',
+        'city': 'Detroit',
+        'name': 'Pistons',
+    }, 'GSW': {
+        'img': 'img/gsw.png',
+        'city': 'Golden State',
+        'name': 'Warriors',
+    }, 'HOU': {
+        'img': 'img/hou.png',
+        'city': 'Houston',
+        'name': 'Rockets',
+    }, 'IND': {
+        'img': 'img/ind.png',
+        'city': 'Indiana',
+        'name': 'Pacers',
+    }, 'LAC': {
+        'img': 'img/lac.png',
+        'city': 'Los Angeles',
+        'name': 'Clippers',
+    }, 'LAL': {
+        'img': 'img/lal.png',
+        'city': 'Los Angeles',
+        'name': 'Lakers',
+    }, 'MEM': {
+        'img': 'img/mem.png',
+        'city': 'Memphis',
+        'name': 'Grizzlies',
+    }, 'MIA': {
+        'img': 'img/mia.png',
+        'city': 'Miami',
+        'name': 'Heat',
+    }, 'MIL': {
+        'img': 'img/mil.png',
+        'city': 'Milwaukee',
+        'name': 'Bucks',
+    }, 'MIN': {
+        'img': 'img/min.png',
+        'city': 'Minnesota',
+        'name': 'Timberwolves',
+    }, 'NOP': {
+        'img': 'img/nop.png',
+        'city': 'New Orleans',
+        'name': 'Pelicans',
+    }, 'NYK': {
+        'img': 'img/nyk.png',
+        'city': 'New York',
+        'name': 'Knicks',
+    }, 'OKC': {
+        'img': 'img/okc.png',
+        'city': 'Oklahoma City',
+        'name': 'Thunder',
+    }, 'ORL': {
+        'img': 'img/orl.png',
+        'city': 'Orlando',
+        'name': 'Magic',
+    }, 'PHI': {
+        'img': 'img/phi.png',
+        'city': 'Philadelphia',
+        'name': '76ers',
+    }, 'PHX': {
+        'img': 'img/phx.png',
+        'city': 'Phoenix',
+        'name': 'Suns',
+    }, 'POR': {
+        'img': 'img/por.png',
+        'city': 'Portland',
+        'name': 'Trailblazers',
+    }, 'SAC': {
+        'img': 'img/sac.png',
+        'city': 'Sacramento',
+        'name': 'Kings',
+    }, 'SAS': {
+        'img': 'img/sas.png',
+        'city': 'San Antonio',
+        'name': 'Spurs',
+    }, 'TOR': {
+        'img': 'img/tor.png',
+        'city': 'Chicago',
+        'name': 'Raptors',
+    }, 'UTA': {
+        'img': 'img/uta.png',
+        'city': 'Utah',
+        'name': 'Jazz',
+    }, 'WAS': {
+        'img': 'img/was.png',
+        'city': 'Washington',
+        'name': 'Wizards',
+    }
+}
+
+CITY_TO_TEAM = {
+    'Atlanta': {
+        'team-name': 'Atlanta Hawks',
+        'abbrev': 'ATL',
+        'img': 'img/atl.png',
+    }, 'Boston': {
+        'team-name': 'Boston Celtics',
+        'abbrev': 'BOS',
+        'img': 'img/bos.png',
+    }, 'Brooklyn': {
+        'team-name': 'Brooklyn Nets',
+        'abbrev': 'BKN',
+        'img': 'img/bkn.png',
+    }, 'Charlotte': {
+        'team-name': 'Charlotte Hornets',
+        'abbrev': 'CHA',
+        'img': 'img/cha.png',
+    }, 'Chicago': {
+        'team-name': 'Chicago Bulls',
+        'abbrev': 'CHI',
+        'img': 'img/chi.png',
+    }, 'Cleveland': {
+        'team-name': 'Cleveland Cavaliers',
+        'abbrev': 'CLE',
+        'img': 'img/cle.png',
+    }, 'Dallas': {
+        'team-name': 'Dallas Mavericks',
+        'abbrev': 'DAL',
+        'img': 'img/dal.png',
+    }, 'Denver': {
+        'team-name': 'Denver Nuggets',
+        'abbrev': 'DEN',
+        'img': 'img/den.png',
+    }, 'Detroit': {
+        'team-name': 'Detroit Pistons',
+        'abbrev': 'DET',
+        'img': 'img/det.png',
+    }, 'Golden State': {
+        'team-name': 'Golden State Warriors',
+        'abbrev': 'GSW',
+        'img': 'img/gsw.png',
+    }, 'Houston': {
+        'team-name': 'Houston Rockets',
+        'abbrev': 'HOU',
+        'img': 'img/hou.png',
+    }, 'Indiana': {
+        'team-name': 'Indiana Pacers',
+        'abbrev': 'IND',
+        'img': 'img/ind.png',
+    }, 'LA Clippers': {
+        'team-name': 'LA Clippers',
+        'abbrev': 'LAC',
+        'img': 'img/lac.png',
+    }, 'L.A. Clippers': {
+        'team-name': 'LA Clippers',
+        'abbrev': 'LAC',
+        'img': 'img/lac.png',
+    }, 'L.A. Lakers': {
+        'team-name': 'LA Lakers',
+        'abbrev': 'LAL',
+        'img': 'img/lal.png',
+    }, 'Memphis': {
+        'team-name': 'Memphis Grizzlies',
+        'abbrev': 'MEM',
+        'img': 'img/mem.png',
+    }, 'Miami': {
+        'team-name': 'Miami Heat',
+        'abbrev': 'MIA',
+        'img': 'img/mia.png',
+    }, 'Milwaukee': {
+        'team-name': 'Milwaukee Bucks',
+        'abbrev': 'MIL',
+        'img': 'img/mil.png',
+    }, 'Minnesota': {
+        'team-name': 'Minnesota Timberwolves',
+        'abbrev': 'MIN',
+        'img': 'img/min.png',
+    }, 'New Orleans': {
+        'team-name': 'New Orleans Pelicans',
+        'abbrev': 'NOP',
+        'img': 'img/nop.png',
+    }, 'New York': {
+        'team-name': 'New York Knicks',
+        'abbrev': 'NYK',
+        'img': 'img/nyk.png',
+    }, 'Oklahoma City': {
+        'team-name': 'OKC Thunder',
+        'abbrev': 'OKC',
+        'img': 'img/okc.png',
+    }, 'Orlando': {
+        'team-name': 'Orlando Magic',
+        'abbrev': 'ORL',
+        'img': 'img/orl.png',
+    }, 'Philadelphia': {
+        'team-name': 'Philadelphia 76ers',
+        'abbrev': 'PHI',
+        'img': 'img/phi.png',
+    }, 'Phoenix': {
+        'team-name': 'Phoenix Suns',
+        'abbrev': 'PHX',
+        'img': 'img/phx.png',
+    }, 'Portland': {
+        'team-name': 'Portland Trailblazers',
+        'abbrev': 'POR',
+        'img': 'img/por.png',
+    }, 'Sacramento': {
+        'team-name': 'Sacramento Kings',
+        'abbrev': 'SAC',
+        'img': 'img/sac.png',
+    }, 'San Antonio': {
+        'team-name': 'San Antonio Spurs',
+        'abbrev': 'SAS',
+        'img': 'img/sas.png',
+    }, 'Toronto': {
+        'team-name': 'Toronto Raptors',
+        'abbrev': 'TOR',
+        'img': 'img/tor.png',
+    }, 'Utah': {
+        'team-name': 'Utah Jazz',
+        'abbrev': 'UTA',
+        'img': 'img/uta.png',
+    }, 'Washington': {
+        'team-name': 'Washington Wizards',
+        'abbrev': 'WAS',
+        'img': 'img/was.png',
+    }
+}
+
+TEAM_ID_TO_NAME = {
+    '1610612737': {
+        'team-name': 'Atlanta Hawks',
+        'abbrev': 'ATL',
+        'img': 'img/atl.png',
+    }, '1610612738': {
+        'team-name': 'Boston Celtics',
+        'abbrev': 'BOS',
+        'img': 'img/bos.png',
+    }, '1610612751': {
+        'team-name': 'Brooklyn Nets',
+        'abbrev': 'BKN',
+        'img': 'img/bkn.png',
+    }, '1610612766': {
+        'team-name': 'Charlotte Hornets',
+        'abbrev': 'CHA',
+        'img': 'img/cha.png',
+    }, '1610612741': {
+        'team-name': 'Chicago Bulls',
+        'abbrev': 'CHI',
+        'img': 'img/chi.png',
+    }, '1610612739': {
+        'team-name': 'Cleveland Cavaliers',
+        'abbrev': 'CLE',
+        'img': 'img/cle.png',
+    }, '1610612742': {
+        'team-name': 'Dallas Mavericks',
+        'abbrev': 'DAL',
+        'img': 'img/dal.png',
+    }, '1610612743': {
+        'team-name': 'Denver Nuggets',
+        'abbrev': 'DEN',
+        'img': 'img/den.png',
+    }, '1610612765': {
+        'team-name': 'Detroit Pistons',
+        'abbrev': 'DET',
+        'img': 'img/det.png',
+    }, '1610612744': {
+        'team-name': 'Golden State Warriors',
+        'abbrev': 'GSW',
+        'img': 'img/gsw.png',
+    }, '1610612745': {
+        'team-name': 'Houston Rockets',
+        'abbrev': 'HOU',
+        'img': 'img/hou.png',
+    }, '1610612754': {
+        'team-name': 'Indiana Pacers',
+        'abbrev': 'IND',
+        'img': 'img/ind.png',
+    }, '1610612746': {
+        'team-name': 'LA Clippers',
+        'abbrev': 'LAC',
+        'img': 'img/lac.png',
+    }, '1610612747': {
+        'team-name': 'LA Lakers',
+        'abbrev': 'LAL',
+        'img': 'img/lal.png',
+    }, '1610612763': {
+        'team-name': 'Memphis Grizzlies',
+        'abbrev': 'MEM',
+        'img': 'img/mem.png',
+    }, '1610612748': {
+        'team-name': 'Miami Heat',
+        'abbrev': 'MIA',
+        'img': 'img/mia.png',
+    }, '1610612749': {
+        'team-name': 'Milwaukee Bucks',
+        'abbrev': 'MIL',
+        'img': 'img/mil.png',
+    }, '1610612750': {
+        'team-name': 'Minnesota Timberwolves',
+        'abbrev': 'MIN',
+        'img': 'img/min.png',
+    }, '1610612740': {
+        'team-name': 'New Orleans Pelicans',
+        'abbrev': 'NOP',
+        'img': 'img/nop.png',
+    }, '1610612752': {
+        'team-name': 'New York Knicks',
+        'abbrev': 'NYK',
+        'img': 'img/nyk.png',
+    }, '1610612760': {
+        'team-name': 'OKC Thunder',
+        'abbrev': 'OKC',
+        'img': 'img/okc.png',
+    }, '1610612753': {
+        'team-name': 'Orlando Magic',
+        'abbrev': 'ORL',
+        'img': 'img/orl.png',
+    }, '1610612755': {
+        'team-name': 'Philadelphia 76ers',
+        'abbrev': 'PHI',
+        'img': 'img/phi.png',
+    }, '1610612756': {
+        'team-name': 'Phoenix Suns',
+        'abbrev': 'PHX',
+        'img': 'img/phx.png',
+    }, '1610612757': {
+        'team-name': 'Portland Trailblazers',
+        'abbrev': 'POR',
+        'img': 'img/por.png',
+    }, '1610612758': {
+        'team-name': 'Sacramento Kings',
+        'abbrev': 'SAC',
+        'img': 'img/sac.png',
+    }, '1610612759': {
+        'team-name': 'San Antonio Spurs',
+        'abbrev': 'SAS',
+        'img': 'img/sas.png',
+    }, '1610612761': {
+        'team-name': 'Toronto Raptors',
+        'abbrev': 'TOR',
+        'img': 'img/tor.png',
+    }, '1610612762': {
+        'team-name': 'Utah Jazz',
+        'abbrev': 'UTA',
+        'img': 'img/uta.png',
+    }, '1610612764': {
+        'team-name': 'Washington Wizards',
+        'abbrev': 'WAS',
+        'img': 'img/was.png',
+    }
+}
