0aa819f40953e503ba6b1c2961b32971b5bb63a9
Alex Cheng <acheng1230@gmail.com>
2020-08-28 15:50:50 -0700

    updates

---
+++ Capstone_Project/app/app/static/js/datepicker.js
@@ -0,0 +1,5 @@
+// https://gijgo.com/datepicker/example/bootstrap-4
+
+$('#datepicker').datepicker({
+  uiLibrary: 'bootstrap4'
+});
--- Capstone_Project/app/app/static/js/plotly.js
+++
@@ -1,7 +0,0 @@
-var figures = {{figuresJSON | safe}};
-  var ids = {{ids | safe}};
-  for(var i in figures) {
-      Plotly.plot(ids[i],
-          figures[i].data,
-          figures[i].layout || {});
-  }
